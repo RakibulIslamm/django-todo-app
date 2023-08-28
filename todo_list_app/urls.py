0efac01c2898e1182b22e0a9735332c2f2d84bcf
MR RAKIB <rakibulislamsofficial@gmail.com>
2023-08-28 14:41:11 +0600

    url name changed

--- todo_list_app/urls.py
+++ todo_list_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('add-task/', views.AddTask, name='add_task'),
     path('show-tasks/', views.ShowTask, name='show_tasks'),
     path('completed-tasks/', views.ShowCompletedTask, name='completed_tasks'),
-    path('edit-task/<int:id>', views.EditTask, name='edit_tasks'),
-    path('complete-task/<int:id>', views.TaskCompleted, name='complete_tasks'),
-    path('delete-task/<int:id>', views.DeleteTask, name='delete_tasks'),
+    path('edit-task/<int:id>', views.EditTask, name='edit_task'),
+    path('complete-task/<int:id>', views.TaskCompleted, name='complete_task'),
+    path('delete-task/<int:id>', views.DeleteTask, name='delete_task'),
 ]
